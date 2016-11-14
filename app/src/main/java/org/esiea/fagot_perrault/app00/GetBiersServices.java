7f5f113184331e0defeeebb4e38ae6a448cfea36
Effey46 <afagot92@gmail.com>
2016-11-14 18:06:10 +0100

    implémentation d'un service de téléchargement

---
+++ app/src/main/java/org/esiea/fagot_perrault/app00/GetBiersServices.java
@@ -0,0 +1,97 @@
+package org.esiea.fagot_perrault.app00;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class GetBiersServices extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_GET_ALL_BIERS = "org.esiea.fagot_perrault.app00.action.FOO";
+    public static final String TAG="GetBiersServices";
+    public GetBiersServices() {
+        super("GetBiersServices");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionGetBiers(Context context) {
+        Intent intent = new Intent(context, GetBiersServices.class);
+        intent.setAction(ACTION_GET_ALL_BIERS);
+        context.startService(intent);
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_GET_ALL_BIERS.equals(action)) {
+                handleActionGetBiers();
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionGetBiers() {
+        Log.i(TAG,"handleActionGetBiers");
+        URL url=null;
+        try {
+            url = new URL("http://binouze.fabrigli.fr/bieres.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if (HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                copyInputStreamToFile(conn.getInputStream(), new File(getCacheDir(), "bieres.json"));
+                Log.d(TAG, "Bieres json downloaded !");
+            }
+            else{
+                Log.w(TAG,"erreur"+conn.getResponseCode());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void copyInputStreamToFile(InputStream in,File file){
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/org/esiea/fagot_perrault/app00/MainActivity.java
+++ app/src/main/java/org/esiea/fagot_perrault/app00/MainActivity.java
@@ -1,13 +1,46 @@
 package org.esiea.fagot_perrault.app00;
 
+import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public DatePickerDialog dpd=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvhello=(TextView)findViewById(R.id.tvhello);
+        final Button btnhello=(Button)findViewById(R.id.btnhello);
+        DatePickerDialog.OnDateSetListener odsl=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                btnhello.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+            }
+        };
+        dpd=new DatePickerDialog(this, odsl,2016,05,23);
+    }
+
+
+    public void boum(View v)
+    {
+        Toast.makeText(getApplicationContext(),getString(R.string.boum),Toast.LENGTH_LONG).show();
+        //dpd.show();
+        //NotificationCompat.Builder builder=(NotificationCompat.Builder)new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher).setContentText("plop").setContentTitle("first plop");
+        //NotificationManager nm=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //nm.notify(0,builder.build());
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+        GetBiersServices.startActionGetBiers(this);
+
     }
 }
